7811edc971a227b544b56ef99a0e4805ff5b097e
jpGonzalez23 <juanpigonzalez72@gmai.com>
2024-05-20 20:08:07 -0300

    Se agrega modificaciones al index.html y styless.css

---
+++ js/planeta.js
@@ -0,0 +1,11 @@
+import PlanetaBase from "./planetaBase";
+
+export default class Planeta extends PlanetaBase {
+    constructor(id, nombre, tamanio, masa, tipo, distanciaSol, poseeAnillo, poseeVida, compocionAtmosferica) {
+        super(id, nombre, tamanio, masa, tipo);
+        this.distanciaSol = distanciaSol;
+        this.poseeAnillo = poseeAnillo;
+        this.poseeVida = poseeVida;
+        this.compocionAtmosferica = compocionAtmosferica;
+    }
+}
---
+++ js/planetaBase.js
@@ -0,0 +1,9 @@
+export default class PlanetaBase {
+    constructor (id, nombre, tamanio, masa, tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tamanio = tamanio;
+        this.masa = masa;
+        this.tipo = tipo;
+    }
+}
